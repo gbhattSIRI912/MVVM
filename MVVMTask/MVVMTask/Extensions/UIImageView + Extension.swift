f3c961d75411af918f023f06eb6c86f9b97c1efa
bhatt.gaurav <bhatt.gaurav@orangemantra.in>
2024-05-12 11:54:25 +0530

    Solve Kingfisher issue

--- MVVMTask/MVVMTask/Extensions/UIImageView + Extension.swift
+++ MVVMTask/MVVMTask/Extensions/UIImageView + Extension.swift
@@ -11,11 +11,11 @@ import UIKit
 
 extension UIImageView {
     func setImage(with urlString: String) {
-        guard let url = URL.init(string: urlString) else {
+        guard let url = URL(string: urlString) else {
             return
         }
-        let resource = ImageResource(downloadURL: url, cacheKey: urlString)
         kf.indicatorType = .activity
-        kf.setImage(with: resource)
+        kf.setImage(with: url)
     }
 }
+
